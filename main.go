9521a6c2ed5a7c3207290964bb5882b4edca71bc
wangyuheng <wangyuheng@outlook.com>
2022-04-08 03:52:22 +0800

    timer sync

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"github.com/geeklubcn/lark/bitable"
+	"github.com/geeklubcn/lark/config"
+	"github.com/geeklubcn/lark/define"
+	"github.com/geeklubcn/lark/gitlab"
+	"github.com/geeklubcn/lark/syncer"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+func main() {
+	cfg := config.Load()
+	g := gitlab.NewGitlabClient(cfg.Domain, cfg.Token, cfg.IssueLabel)
+	b := bitable.NewBitable(cfg.AppId, cfg.AppSecret, cfg.AppToken)
+	s := syncer.NewSyncer(b, g)
+
+	dsl, _ := define.NewParser().Parse("./define.yaml")
+	_ = s.SyncSchemaToRemoteBitable(context.Background(), &dsl)
+
+	c := make(chan int, 1)
+	q := make(chan int, 1)
+	go listen(c)
+	go func() {
+		for {
+			select {
+			case <-time.Tick(cfg.SyncPeriod):
+				logrus.Debug("============start sync============")
+				ctx := context.Background()
+				if err := s.Sync(ctx); err != nil {
+					logrus.WithContext(ctx).Error("sync fail!")
+				}
+				logrus.Debug("============end sync============")
+			}
+		}
+	}()
+	<-q
+}
+
+func listen(_ chan int) {
+	//TODO handle hook
+}
