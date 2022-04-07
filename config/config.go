3921342709a835fc87cbdf7cc2dc468c851c3b12
wangyuheng <wangyuheng@outlook.com>
2022-04-08 00:43:13 +0800

    config by env

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import "github.com/spf13/viper"
+
+var cfg = &Config{}
+
+const (
+	LarkAppId           = "LARK_APP_ID"
+	LarkAppSecret       = "LARK_APP_SECRET"
+	LarkBitableAppToken = "LARK_BITABLE_APP_TOKEN"
+
+	LarkGitlabDomain     = "LARK_GITLAB_DOMAIN"
+	LarkGitlabToken      = "LARK_GITLAB_TOKEN"
+	LarkGitlabIssueLabel = "LARK_GITLAB_ISSUE_LABEL"
+)
+
+type Config struct {
+	AppId     string
+	AppSecret string
+	GitlabConfig
+	BitableConfig
+}
+
+type BitableConfig struct {
+	AppToken string
+}
+type GitlabConfig struct {
+	Domain     string
+	Token      string
+	IssueLabel string
+}
+
+func Load() {
+	v := viper.New()
+	v.AutomaticEnv()
+
+	_ = v.BindEnv(LarkGitlabDomain)
+	_ = v.BindEnv(LarkGitlabToken)
+	_ = v.BindEnv(LarkGitlabIssueLabel)
+	cfg.GitlabConfig.Domain = v.GetString(LarkGitlabDomain)
+	cfg.GitlabConfig.Token = v.GetString(LarkGitlabToken)
+	cfg.GitlabConfig.IssueLabel = v.GetString(LarkGitlabIssueLabel)
+
+	_ = v.BindEnv(LarkAppId)
+	_ = v.BindEnv(LarkAppSecret)
+	_ = v.BindEnv(LarkBitableAppToken)
+	cfg.AppId = v.GetString(LarkAppId)
+	cfg.AppSecret = v.GetString(LarkAppSecret)
+	cfg.BitableConfig.AppToken = v.GetString(LarkBitableAppToken)
+}
+
+func GetConfig() *Config {
+	return cfg
+}
---
+++ config/config_test.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+
+	t.Run("should get cfg val after Load", func(t *testing.T) {
+		_ = os.Setenv(LarkGitlabToken, "abc")
+		defer func() { _ = os.Unsetenv(LarkGitlabToken) }()
+		assert.Equal(t, "", GetConfig().Token)
+		Load()
+		assert.Equal(t, "abc", GetConfig().Token)
+	})
+
+}
